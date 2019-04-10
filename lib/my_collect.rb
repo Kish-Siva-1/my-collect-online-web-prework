e76eb328553be3efa462d26760bcbe39dcc81273
Kish Siva <rodes88@msn.com>
2019-04-10 11:16:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,9 @@ def my_collect(array_input)
   arr=[]
   counter = 0 
   while counter<array_input.length 
-    yield array_input[counter]
+    arr[counter]=[yield array_input[counter]]
     counter+=1
   end
-  
-  array_input
   
 end
 
